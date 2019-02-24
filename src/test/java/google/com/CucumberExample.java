a8fbdd65acc4850aa4601c3730c4ada5c5342300
john <john@user>
2019-02-24 13:10:12 +0530

    this is commit

---
+++ src/test/java/google/com/CucumberExample.java
@@ -0,0 +1,53 @@
+package google.com;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CucumberExample {
+	@Given("^I want to write a step with precondition$")
+	public void i_want_to_write_a_step_with_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	    System.out.println("xgufgjklkj");
+	}
+
+	@Given("^some other precondition$")
+	public void some_other_precondition()  {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("xgufgjklkj");
+	}
+
+	@When("^I complete action$")
+	public void i_complete_action() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("xgufgjklkj");
+	}
+
+	@When("^some other action$")
+	public void some_other_action() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("xgufgjklkj");
+	}
+
+	@When("^yet another action$")
+	public void yet_another_action(){
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("xgufgjklkj");
+	}
+
+	@Then("^I validate the outcomes$")
+	public void i_validate_the_outcomes(){
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("xgufgjklkj");
+	}
+
+	@Then("^check more outcomes$")
+	public void check_more_outcomes() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("xgufgjklkj");
+	}
+
+
+
+}
---
+++ src/test/java/google/com/TestRunner.java
@@ -0,0 +1,16 @@
+package google.com;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions( 
+features="C:\\Users\\john\\Desktop\\Selenium\\TestNG Workspace\\"
+		+ "FacebookExample\\src\\test\\resources\\Featur File\\"
+		+ "cucumber.feature")
+
+public class TestRunner {
+
+}
